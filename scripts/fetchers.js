2640140cab5ea30d8699cbe95b31577a9d78344c
Surmon <surmon.me@gmail.com>
2022-07-21 12:34:34 +0800

    Update fetchers.js

--- scripts/fetchers.js
+++ scripts/fetchers.js
@@ -8,11 +8,13 @@ axios.interceptors.response.use(
   }
 )
 
+// https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md
 exports.fetchNPMPackages = async (npmUID) => {
   const response = await axios.get(`https://registry.npmjs.com/-/v1/search?text=maintainer:${npmUID}`)
   return response.data.objects
 }
 
+// https://github.com/npm/registry/blob/master/docs/download-counts.md
 exports.fetchNPMPackageDownloads = async (packageName) => {
   const now = new Date()
   const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
